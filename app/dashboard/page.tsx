aa8ba4d07659e48d87c942df714f520266b85657
hshoja <hamid.shoja@gmail.com>
2023-11-13 21:31:59 +0100

    Chapter8: Static/Dynamic rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data";
-import { sql } from "@vercel/postgres";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
