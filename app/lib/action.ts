573db721ef1cf537692f540460afb0fe4e00e971
Hamid Shoja <hamid.shoja@gmail.com>
2023-12-02 22:54:54 +0100

    Chapter 17: Auth

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { getUser } from "./data";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -98,7 +101,6 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
@@ -106,5 +108,24 @@ export async function deleteInvoice(id: string) {
     return {
       message: "Database Error: Failed to Delete Invoice.",
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
